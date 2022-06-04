de54952b11372234558a9edd9749584e20301c04
ILIDZH <ilias.dzhumabekov@gmail.com>
2022-06-04 22:50:02 +0200

    задача про температуры

---
+++ src/test3.java
@@ -0,0 +1,13 @@
+
+import java.util.Arrays;
+
+public class test3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[] temp={10, 11, 88, 2, 12, 16, 18}; 
+	    Arrays.sort(temp);
+	    System.out.println("Max temp: "+temp[6]);
+	}
+
+}
