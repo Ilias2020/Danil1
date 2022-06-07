970c6425f1cab1359fba7f68d92e5154ef3e943f
ILIDZH <ilias.dzhumabekov@gmail.com>
2022-06-07 16:16:19 +0200

    Now the code takes into account if the number of votes in the voting matches.

--- src/test2.java
+++ src/test2.java
@@ -1,14 +1,13 @@
 
 public class test2 {
-			public static void main(String[] args) {
-			// TODO Auto-generated method stub
-			
-			
-			int i = 190;
-			while (i%17!=0)		i++;
-			
-			System.out.println(i);
-			
-	}
-	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
 
+		int i = 190;
+		while (i % 17 != 0)
+			i++;
+
+		System.out.println(i);
+
+	}
+}
--- src/test4.java
+++ src/test4.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+
 public class test4 {
 
 	public static void main(String[] args) {
@@ -8,39 +9,39 @@ public class test4 {
 		double num1 = in1.nextDouble();
 		System.out.print("Skolko golosov u Tashieva= ");
 		Scanner in2 = new Scanner(System.in);
-		double num2 =in2.nextDouble();
+		double num2 = in2.nextDouble();
 		System.out.print("Skolko golosov u Toktogazieva= ");
 		Scanner in3 = new Scanner(System.in);
-		double num3 =in3.nextDouble();
+		double num3 = in3.nextDouble();
 		in1.close();
 		in2.close();
 		in3.close();
-		if ((num1 == num2) || (num1 == num3)|| (num2==num3)) {
+		if ((num1 == num2) || (num1 == num3) || (num2 == num3)) {
 			if (num1 > num2 && num1 > num3) {
 				System.out.println("Taschiev i Toktogayiev proigrali ");
 			} else if (num2 > num3 && num2 > num1) {
 				System.out.println("Zaparov i Toktogayiev proigrali ");
 			} else if (num3 > num1 && num3 > num2) {
-				System.out.println("Zaparov i Taschiev proigrali ");;
-			} else { 
-			System.out.println("Neobchodim vtoroi tur,");
-			if (num1 < num2 && num1 < num3) {
-				System.out.println("a v pervom ture Zhaparova proigral ");
-			} else if (num2 < num3 && num2 < num1) {
-				System.out.println("a v pervom ture Taschiev proigral ");
+				System.out.println("Zaparov i Taschiev proigrali ");
+				;
 			} else {
-				System.out.println("a v pervom ture Toktogayiev proigral ");;
+				System.out.println("Neobchodim vtoroi tur,");
+				if (num1 < num2 && num1 < num3) {
+					System.out.println("a v pervom ture Zhaparova proigral ");
+				} else if (num2 < num3 && num2 < num1) {
+					System.out.println("a v pervom ture Taschiev proigral ");
+				} else {
+					System.out.println("a v pervom ture Toktogayiev proigral ");
+				}
 			}
-			}
-		}
-		else {
-		if (num1 < num2 && num1 < num3) {
-			System.out.println("Zhaparova proigral ");
-		} else if (num2 < num3 && num2 < num1) {
-			System.out.println("Taschiev proigral ");
 		} else {
-			System.out.println("Toktogayiev proigral ");;
-		}
+			if (num1 < num2 && num1 < num3) {
+				System.out.println("Zhaparova proigral ");
+			} else if (num2 < num3 && num2 < num1) {
+				System.out.println("Taschiev proigral ");
+			} else {
+				System.out.println("Toktogayiev proigral ");
+			}
 		}
 	}
 
